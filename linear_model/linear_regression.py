d10d26a9a9806945fcae608e4579edc38d00d1c9
ljwsummer <ljwsummer@gmail.com>
2012-12-11 19:50:30 +0800

    init *linear_model* for all linear models

---
+++ linear_model/linear_regression.py
@@ -0,0 +1,29 @@
+# -*-coding:utf-8-*-
+
+import numpy as np
+
+class LinearReg(object):
+    """linear regression"""
+
+    def __init__(self):
+        self.model = np.matrix('')
+
+    def _compute_cost(self):
+        pass
+
+    def _compute_gradient(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def save_model(self):
+        pass
+
+    def load_model(self):
+        pass
+
+
