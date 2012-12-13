936fefa86b68a5eb3b9750282dfbe140c09eeba3
ljwsummer <ljwsummer@gmail.com>
2012-12-13 13:43:38 +0800

    use test class

--- unit_tests/common/test_reader.py
+++ unit_tests/common/test_reader.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*-coding:utf-8-*-
 
 import os
@@ -6,37 +5,34 @@ import os
 from unit_tests import config
 from mlsoft.common import reader
 
-def setUp():
-    pass
+class TestData(object):
 
-def tearDown():
-    pass
+    def setup(self):
+        self.path = os.path.join(config.test_data_path, 'test_data.txt')
+        self.data = reader.Data(self.path)
 
+    def teardown(self):
+        del self.data
 
-def test_append_line():
-    path = os.path.join(config.test_data_path, 'test_data.txt')
-    data = reader.Data(path)
-    m, n = data.X.shape
-    data.append_line('1 qid:2 1:0.21 3:0.11 27:-0.32')
-    m1, n1 = data.X.shape
-    assert m1 == m + 1
-    assert n1 == n
-    assert data.Y[m, 0] == 1
-    assert data.qid[m, 0] == 2
-    assert data.X[m, 0] == 0.21
-    assert data.X[m, 2] == 0.11
-    assert data.X[m, 26] == -0.32
-    for x in xrange(n):
-        if x not in [0, 2, 26]:
-            assert data.X[m, x] == 0
+    def test_append_line(self):
+        m, n = self.data.X.shape
+        self.data.append_line('1 qid:2 1:0.21 3:0.11 27:-0.32')
+        m1, n1 = self.data.X.shape
+        assert m1 == m + 1
+        assert n1 == n
+        assert self.data.Y[m, 0] == 1
+        assert self.data.qid[m, 0] == 2
+        assert self.data.X[m, 0] == 0.21
+        assert self.data.X[m, 2] == 0.11
+        assert self.data.X[m, 26] == -0.32
+        for x in xrange(n):
+            if x not in [0, 2, 26]:
+                assert self.data.X[m, x] == 0
 
-
-def test_append_file():
-    path = os.path.join(config.test_data_path, 'test_data.txt')
-    data = reader.Data(path)
-    m, n = data.X.shape
-    data.append_file(path)
-    assert (data.X[:m] == data.X[m:]).all()
-    assert (data.Y[:m] == data.Y[m:]).all()
-    assert (data.qid[:m] == data.qid[m:]).all()
+    def test_append_file(self):
+        m, n = self.data.X.shape
+        self.data.append_file(self.path)
+        assert (self.data.X[:m] == self.data.X[m:]).all()
+        assert (self.data.Y[:m] == self.data.Y[m:]).all()
+        assert (self.data.qid[:m] == self.data.qid[m:]).all()
 
