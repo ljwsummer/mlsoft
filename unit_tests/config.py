ca86ca2628f8ffbdd1e506c952c3e3f95c3604a3
ljwsummer <ljwsummer@gmail.com>
2013-01-28 15:13:13 +0800

    update unit test config

--- unit_tests/config.py
+++ unit_tests/config.py
@@ -1,4 +1,12 @@
 # -*-coding:utf-8-*-
+import os
 
 unit_tests_path = './unit_tests/'
 test_data_path = './unit_tests/data/'
+
+model_conf = './unit_tests/conf/'
+
+linear_reg_model = os.path.join(test_data_path, 'test_data.linear_reg_model')
+linear_reg_costs = os.path.join(test_data_path,'test_data.linear_reg_costs')
+linear_reg_predicts = os.path.join(test_data_path,'test_data.linear_reg_predicts')
+
