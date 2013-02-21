4e5d9cb24eca73555eec5cac337253b6e2fef9ab
ljwsummer <ljwsummer@gmail.com>
2013-02-21 15:10:18 +0800

    let LinearReg inherit the LinearModel class

--- mlsoft/linear_model/linear_regression.py
+++ mlsoft/linear_model/linear_regression.py
@@ -3,32 +3,17 @@
 __author__ = "Jiawang Liu (ljwsummer@gmail.com)"
 __date__ = "2012/12"
 
-import ConfigParser
 import numpy as np
-import mlsoft.common.reader
+from linear_model import LinearModel
 
-class LinearReg(object):
+class LinearReg(LinearModel):
     """linear regression"""
 
     def __init__(self, conf_file):
-        self._load_conf(conf_file)
+        LinearModel.__init__(self, conf_file)
         self.alpha = self.conf.getfloat('linear_regression', 'alpha')
         self.iters = self.conf.getint('linear_regression', 'num_iters')
         self.lambd = self.conf.getfloat('linear_regression', 'lambda')
-        data_file = self.conf.get('global', 'train_data')
-        self._load_data(data_file)
-        self.m = self.data.X.shape[0]
-        self.n = self.data.X.shape[1]
-        self.theta = np.mat(np.zeros((self.n, 1)))
-        self.costs = []
-
-    def _load_conf(self, conf_file):
-        self.conf = ConfigParser.ConfigParser()
-        self.conf.optionxform = str
-        self.conf.read(conf_file)
-
-    def _load_data(self, data_file):
-        self.data = mlsoft.common.reader.Data(data_file)
 
     def _compute_cost(self, X, Y, theta):
         m = X.shape[0]
@@ -42,12 +27,6 @@ class LinearReg(object):
     def _gradient_descent(self):
         grad = self._compute_gradient(self.data.X, self.data.Y, self.theta)
         self.theta -= self.alpha * grad / self.m
-
-    def train(self):
-        for x in xrange(self.iters):
-            self._gradient_descent()
-            cost = self._compute_cost(self.data.X, self.data.Y, self.theta)
-            self.costs.append(cost)
 
     def predict(self, X):
         delta = X.shape[1] - self.theta.shape[0]
