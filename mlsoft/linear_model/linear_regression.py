f2825a9f2d56edc7f7b6ad50ed13217cf3f992c9
ljwsummer <ljwsummer@gmail.com>
2013-02-22 13:11:18 +0800

    support *intercept* in linear regression

--- mlsoft/linear_model/linear_regression.py
+++ mlsoft/linear_model/linear_regression.py
@@ -14,15 +14,22 @@ class LinearReg(LinearModel):
         self.alpha = self.conf.getfloat('linear_regression', 'alpha')
         self.iters = self.conf.getint('linear_regression', 'num_iters')
         self.lambd = self.conf.getfloat('linear_regression', 'lambda')
+        self.intercept = self.conf.getboolean('linear_regression', 'has_intercept')
 
     def _compute_cost(self, X, Y, theta):
         m = X.shape[0]
         res = X * theta - Y
-        j = sum(np.power(res, 2)) / m
+        idx = 1 if self.intercept else 0
+        j = sum(np.power(res, 2)) / m + self.lambd * sum(np.power(theta[idx:], 2)) / (2 * m)
         return j[0,0]
 
     def _compute_gradient(self, X, Y, theta):
-        return X.T * (X * theta - Y) + self.lambd * theta
+        idx = 1 if self.intercept else 0
+        if self.intercept:
+            ret = X.T * (X * theta - Y) + np.vstack([np.matrix('0'), self.lambd * theta[1:]])
+        else:
+            ret = X.T * (X * theta - Y) + self.lambd * theta
+        return ret
 
     def _gradient_descent(self):
         grad = self._compute_gradient(self.data.X, self.data.Y, self.theta)
