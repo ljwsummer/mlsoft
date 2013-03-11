2f5e616b5f5b5e190a3fe123cfcdabd596d7c058
ljwsummer <ljwsummer@gmail.com>
2013-03-11 21:21:09 +0800

    make run_examples.sh and run_tests.sh more robust

--- run_examples.sh
+++ run_examples.sh
@@ -1,6 +1,24 @@
 
-export PYTHONPATH=./
-mkdir examples_output
-python examples/linear_reg_example.py -c examples/model.cfg -m examples_output/model.linear_reg
-python examples/logistic_reg_example.py -c examples/model.cfg -m examples_output/model.logistic_reg
+work=./
+cfg=./examples/model.cfg
+out=./examples_output
+linear_reg_model=$out/model.linear_reg
+logistic_reg_model=$out/model.logistic_reg
+
+if ! echo $PYTHONPATH | egrep -q "(^|:)$work($|:)" 
+then
+    export PYTHONPATH=$PYTHONPATH:$work
+fi
+
+if [ ! -d $out ]
+then
+    mkdir examples_output
+fi
+
+# training model
+python ./examples/linear_reg_example.py -c $cfg -m $linear_reg_model
+python ./examples/logistic_reg_example.py -c $cfg -m $logistic_reg_model
+
+echo "linear regression model: $linear_reg_model"
+echo "logistic regression model: $logistic_reg_model"
 
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,11 @@
 
-export PYTHONPATH=./
+work=./
+
+if ! echo $PYTHONPATH | egrep -q "(^|:)$work($|:)" 
+then
+    export PYTHONPATH=$PYTHONPATH:$work
+fi
+
 nosetests unit_tests/common
 nosetests unit_tests/linear_model
 
